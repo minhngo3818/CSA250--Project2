c3be256c1dc169a23b7ef8253ef26f8785b5cbba
Riana Ali <rianaali728@gmail.com>
2021-12-08 01:20:58 -0800

    isEmpty, searchID, deleteDonor, clearList, but still needs to be debugged

--- Project2/DonorList.cpp
+++ Project2/DonorList.cpp
@@ -15,10 +15,10 @@
 
 #include "DonorList.h"
 #include "DonorType.h"
-
 #include <iostream>
 #include <set>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
@@ -72,17 +72,27 @@ double DonorList::getHighestDonation() const
 
 bool DonorList::isEmpty() const
 {
-
+	return donorList->empty();
 }
 
 bool DonorList::searchID(int membershipNo) const
 {
+	return find(donorList->begin(), donorList->end(), 
+		membershipNo) != donorList->end();
 	
 }
 
 void DonorList::deleteDonor(int membershipNo)
 {
-	
+	auto deleteElem = find(donorList->begin(), donorList->end(),
+		membershipNo);
+	if (deleteElem != donorList->end()) 
+	{
+		donorList->erase(deleteElem);
+	}
+	else {
+		cout << "Donor is not in the list";
+	}
 }
 
 void DonorList::printAllDonors() const
@@ -97,7 +107,7 @@ void DonorList::printAllDonations() const
 
 void DonorList::clearList()
 {
-	
+	donorList->clear();
 }
 
 DonorList::~DonorList()
